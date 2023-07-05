ed527d722925571c3ff7703ca043fcc73110faf1
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-07-04 22:11:44 -0300

    Adiciona Input

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,14 @@
+import gitLogo from "../assets/github_logo.png"
+import Input from "../components/Input";
+
+import { Container } from "./styles";
+
 function App() {
   return (
-    <div>
-      Ola
-    </div>
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt="logo github"/>
+      <Input />
+    </Container>
   );
 }
 
