f931323dacbdeb5426d11283d78d0fa3ca5e3b83
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-07-06 21:15:59 -0300

    Finaliza listagem

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    widht: 100%;
-    height: 90vh;
-    margin: 20px 0 0 0;
+  width: 100%;
+  height: 90vh;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-`
+  margin: 20px 0 0 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+`;
